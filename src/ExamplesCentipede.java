32428de74908cded1476727901efc2836a03d838
tiingweii-shii <57331456+tiingweii-shii@users.noreply.github.com>
2020-03-24 15:16:29 -0400

    Comment on makeScene BGame

--- src/ExamplesCentipede.java
+++ src/ExamplesCentipede.java
@@ -486,6 +486,9 @@ class ExamplesCentipede {
         && t.checkExpect(bgame_1.toCGameState(), new CGameState(3, 3, garden_1, player));
   }
   
+  // we did not test makeScene in the BGameState class, because we can visualize it when we 
+  // run bigBang, and check if things are drawn correctly from there, which they were.
+  
   // function object tests
 
   // tests IsGrass, IsPebble, IsDandelion apply(ITile)
